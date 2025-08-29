1bcb43217002f73f5c7284f8d0d2558763ea057b
Naveenthan N <n.naveenthann@gmail.com>
2025-08-28 23:35:47 -0700

    Update Event.js
    added img url path

--- models/Event.js
+++ models/Event.js
@@ -4,7 +4,8 @@ const eventSchema = new mongoose.Schema({
   name: { type: String, required: true },
   date: { type: Date, required: true },
   location: { type: String, required: true },
-  description: { type: String }
+  description: { type: String },
+  img: { type: String } // image URL
 }, { timestamps: true });
 
 const Event = mongoose.model('Event', eventSchema);
